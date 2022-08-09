6664d1c3339a66e1672d35a67e5ad7019e15fa29
Antonio Leoncio Vieira Neto <antonio.leoncio@funai.gov.br>
2022-08-09 14:45:42 -0300

    Piloto Pronto

---
+++ assets/js/index.js
@@ -0,0 +1,29 @@
+function pull1 () {
+    document.getElementById("conteudo__imagem").src="./assets/img/treinos/pull1.png"
+    console.log('estou sendo pull1')
+}
+
+function pull2 () {
+    document.getElementById("conteudo__imagem").src="./assets/img/treinos/pull2.png"
+    console.log('estou sendo pull2')
+}
+
+function push1 () {
+    document.getElementById("conteudo__imagem").src="./assets/img/treinos/push1.png"
+    console.log('estou sendo push1')
+}
+
+function push2 () {
+    document.getElementById("conteudo__imagem").src="./assets/img/treinos/push2.png"
+    console.log('estou sendo push2')
+}
+
+function legs1 () {
+    document.getElementById("conteudo__imagem").src="./assets/img/treinos/legs1.png"
+    console.log('estou sendo legs1')
+}
+
+function legs2 () {
+    document.getElementById("conteudo__imagem").src="./assets/img/treinos/legs2.png"
+    console.log('estou sendo legs2')
+}
