6dbff37f8ea5da134478d974c97b0bb624efca8c
Antonio Leoncio Vieira Neto <antonio.leoncio@funai.gov.br>
2022-08-10 16:30:41 -0300

    Cronomentro finalizado

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,29 +1,88 @@
+function reload(){
+    document.location.reload(true)
+}
+
+////////
+
 function pull1 () {
-    document.getElementById("conteudo__imagem").src="./assets/img/treinos/pull1.png"
-    console.log('estou sendo pull1')
+    document.getElementById("conteudo__imagem").src="./assets/img/treinos/abc2x/pull1.png"
 }
 
 function pull2 () {
-    document.getElementById("conteudo__imagem").src="./assets/img/treinos/pull2.png"
-    console.log('estou sendo pull2')
+    document.getElementById("conteudo__imagem").src="./assets/img/treinos/abc2x/pull2.png"
 }
 
 function push1 () {
-    document.getElementById("conteudo__imagem").src="./assets/img/treinos/push1.png"
-    console.log('estou sendo push1')
+    document.getElementById("conteudo__imagem").src="./assets/img/treinos/abc2x/push1.png"
 }
 
 function push2 () {
-    document.getElementById("conteudo__imagem").src="./assets/img/treinos/push2.png"
-    console.log('estou sendo push2')
+    document.getElementById("conteudo__imagem").src="./assets/img/treinos/abc2x/push2.png"
 }
 
 function legs1 () {
-    document.getElementById("conteudo__imagem").src="./assets/img/treinos/legs1.png"
-    console.log('estou sendo legs1')
+    document.getElementById("conteudo__imagem").src="./assets/img/treinos/abc2x/legs1.png"
 }
 
 function legs2 () {
-    document.getElementById("conteudo__imagem").src="./assets/img/treinos/legs2.png"
-    console.log('estou sendo legs2')
+    document.getElementById("conteudo__imagem").src="./assets/img/treinos/abc2x/legs2.png"
+}
+
+////////////////////
+
+function abcde1() {
+    document.getElementById("conteudo__imagem").src="./assets/img/treinos/abcde/Delts.png"
+}
+
+function abcde2() {
+    document.getElementById("conteudo__imagem").src="./assets/img/treinos/abcde/Dorsais.png"
+}
+
+function abcde3() {
+    document.getElementById("conteudo__imagem").src="./assets/img/treinos/abcde/MMII.png"
+}
+
+function abcde4() {
+    document.getElementById("conteudo__imagem").src="./assets/img/treinos/abcde/Peitorais.png"
+}
+
+function abcde5() {
+    document.getElementById("conteudo__imagem").src="./assets/img/treinos/abcde/PontosFracos.png"
+}
+
+//
+
+function phat1() {
+    document.getElementById("conteudo__imagem").src="./assets/img/treinos/phat/SupForca.png"
+}
+
+function phat2() {
+    document.getElementById("conteudo__imagem").src="./assets/img/treinos/phat/InfForca.png"
+}
+
+function phat3() {
+    document.getElementById("conteudo__imagem").src="./assets/img/treinos/phat/CostOmb.png"
+}
+
+function phat4() {
+    document.getElementById("conteudo__imagem").src="./assets/img/treinos/phat/InfComplet.png"
+}
+
+function phat5() {
+    document.getElementById("conteudo__imagem").src="./assets/img/treinos/phat/PeitoBracos.png"
+}
+
+//
+
+function ab2() {
+    document.getElementById("conteudo__imagem").src="./assets/img/treinos/AB/Lower.png"
+}
+
+function ab2() {
+    document.getElementById("conteudo__imagem").src="./assets/img/treinos/AB/Upper.png"
+}
+
+// 
+function abs() {
+    document.getElementById("conteudo__imagem").src="./assets/img/treinos/abs.png"
 }
--- assets/js/treinos.js
+++ assets/js/treinos.js
@@ -1,20 +1,61 @@
-var menuTreinos = document.getElementById("treinos");
-menuTreinos.addEventListener("click", function() {
+// var menuTreinos = document.getElementById("treinos");
+// menuTreinos.addEventListener("click", function() {
+//     console.log("clicado")
+//     var tiposDeTreinos = document.getElementById("tiposDeTreinos");
+//     tiposDeTreinos.classList.toggle("mTS_hide");
+// })
+
+// var abc2xbtn = document.getElementById("abc2x__lista");
+// abc2xbtn.addEventListener("click", function() {
+//     console.log("clicado")
+//     var abc2x = document.getElementById("abc2xtreinos");
+//     abc2x.classList.toggle("ABC2x_hide");
+// })
+
+
+// var abcdeBtn = document.getElementById("abcde__lista");
+// abcdeBtn.addEventListener("click", function() {
+//     abc2xbtn.removeEventListener("click", abc2x)
+//     console.log("clicado")
+//     var abcde = document.getElementById("abcdetreinos");
+//     abcde.classList.toggle("abcde_hide");
+// })
+
+const menuTreinos = document.getElementById("treinos");
+const abc2xbtn = document.getElementById("abc2x__lista");
+const abcdeBtn = document.getElementById("abcde__lista");
+const pathBtn = document.getElementById("phat__lista");
+const ABBtn = document.getElementById("ab__lista");
+const uplowerBtn = document.getElementById("uplower__lista");
+
+menuTreinos.addEventListener('click', () => {
     console.log("clicado")
     var tiposDeTreinos = document.getElementById("tiposDeTreinos");
     tiposDeTreinos.classList.toggle("mTS_hide");
+    return true;
 })
 
-var abc2xbtn = document.getElementById("abc2x__lista");
-abc2xbtn.addEventListener("click", function() {
-    console.log("clicado")
+abc2xbtn.addEventListener('click', () => {
     var abc2x = document.getElementById("abc2xtreinos");
     abc2x.classList.toggle("ABC2x_hide");
 })
 
-var abcdeBtn = document.getElementById("abcde__lista");
-abcdeBtn.addEventListener("click", function() {
-    console.log("clicado")
-    var abcde = document.getElementById("abcdetreinos");
-    abcde.classList.toggle("abcde_hide");
+abcdeBtn.addEventListener('click', () => {
+        var abcde = document.getElementById("abcdetreinos");
+        abcde.classList.toggle("abcde_hide");
+})
+
+pathBtn.addEventListener('click', () => {
+    var path = document.getElementById("pathtreinos");
+    path.classList.toggle("path_hide");
+})
+
+ABBtn.addEventListener('click', () => {
+    var ab = document.getElementById("abtreinos");
+    ab.classList.toggle("ab_hide");
+})
+
+uplowerBtn.addEventListener('click', () => {
+    var uplower = document.getElementById("uplowertreinos");
+    uplower.classList.toggle("uplower_hide");
 })
