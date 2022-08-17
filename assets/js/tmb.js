502c3566bfefcccd617e2168543f08b2ecd273c1
Antonio Leoncio Vieira Neto <antonio.leoncio@funai.gov.br>
2022-08-17 11:52:42 -0300

    inicio do tmb

---
+++ assets/js/tmb.js
@@ -0,0 +1,4 @@
+var btn = document.querySelector("#send");
+var conteudoImagem =  document.getElementById("conteudo__imagem")
+var imcId = document.getElementById("imcTexto")
+var imcId2 = document.getElementById("imcTexto2")
